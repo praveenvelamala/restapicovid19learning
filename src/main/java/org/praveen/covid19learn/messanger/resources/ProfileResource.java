5d75653962987e385711a78c7f6968ce373d2826
pvelamal <pvelamal@9TJ0VP2.misys.global.ad>
2020-08-09 19:48:37 +0530

    Adding Profile resource - JavaBrainsKoushik

--- src/main/java/org/praveen/covid19learn/messanger/resources/ProfileResource.java
+++ src/main/java/org/praveen/covid19learn/messanger/resources/ProfileResource.java
@@ -1,5 +1,55 @@
 package org.praveen.covid19learn.messanger.resources;
 
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.praveen.covid19learn.messanger.model.Profile;
+import org.praveen.covid19learn.messanger.service.ProfileService;
+
+@Path ("/profiles")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class ProfileResource {
+
+	ProfileService ps = new ProfileService();
+	
+	
+@GET
+public List<Profile> getAllProfiles(){
+	return ps.getAllProfiles();
+}
+
+@GET
+@Path("/{name}")
+public Profile getProfile(@PathParam("name") String name){
+	System.out.println("In get");
+	return ps.getProfile(name);
+}
+
+@POST
+public Profile createProfile(Profile p){
+	return ps.addProfile(p);	 
+}
+
+@PUT
+@Path("/{name}")
+public Profile updateProfile(@PathParam("name") String name, Profile p){
+	return ps.updateProfile(p);
+}
+
+@DELETE
+@Path("/{name}")
+public void deleteProfile(@PathParam("name") String name){
+	ps.deleteProfile(name);
+}
 
 }
--- src/main/java/org/praveen/covid19learn/messanger/service/ProfileService.java
+++ src/main/java/org/praveen/covid19learn/messanger/service/ProfileService.java
@@ -1,5 +1,7 @@
 package org.praveen.covid19learn.messanger.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.praveen.covid19learn.messanger.database.DatabaseClass;
@@ -8,6 +10,43 @@ import org.praveen.covid19learn.messanger.model.Profile;
 public class ProfileService {
 
 	private Map<String, Profile> profileMap = DatabaseClass.getProfiles();
+
+	public ProfileService(){
+		Profile praveen = new Profile("pvelamal", "praveen", "velamala", 1L, 'M');
+		Profile kalyani = new Profile("kalyaniv", "kalyani", "vana", 2L, 'F');
+		Profile amarDeep = new Profile("amar", "amardeep", "velamala", 3L, 'M');
+		profileMap.put("praveen", praveen);
+		profileMap.put("kalyani", kalyani);
+		profileMap.put("amardeep", amarDeep);
+		
+	}
+	
+public List<Profile> getAllProfiles(){
+	return new ArrayList<Profile>(profileMap.values());
+}
+
+public Profile getProfile(String name){
+	return profileMap.get(name);
+}
+
+public Profile addProfile(Profile p){
+	p.setId((long) (profileMap.size()+1));
+	profileMap.put(p.firstName, p);
+	return profileMap.get(p.firstName);
+}
+
+public Profile updateProfile(Profile p){
+	if(p.getFirstName()!= null){
+	profileMap.put(p.getFirstName(),p);
+	return profileMap.get(p.getFirstName());}
+	else
+		return null;
+	}
+
+public void deleteProfile(String name){
+	profileMap.remove(name);
 }
 
 
+
+}
